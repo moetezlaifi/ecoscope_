5e0af368e7c601078ee776091caae5ac2d291f7c
moetezlaifi <moetez.laifi@gmail.com>
2025-12-20 23:25:34 +0100

    add models to admin

--- ecoscope/core/admin.py
+++ ecoscope/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Site, SiteSignal, RiskSnapshot
 
-# Register your models here.
+admin.site.register(Site)
+admin.site.register(SiteSignal)
+admin.site.register(RiskSnapshot)
